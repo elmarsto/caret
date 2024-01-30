ad109aff4b63e770493562c2c1f4238a2015eb82
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-01-30 00:58:24 -0800

    wip

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,5 @@
 # This is passed into each leaf of the site being built
-{ pkgs }: 
-{
-
+{ pkgs }:  let
   # utilities for manipulating CSS
   css = import ./css.nix { inherit pkgs; };
   
@@ -13,5 +11,29 @@
 
   # utilities for manipulating URLs
   url = import ./url.nix { inherit pkgs; };
+in
+{
+  inherit css html rss url;
+  root = let 
+    identity = s: s;
+    script = s: pkgs.writeTextFile "caret-root-script" s;
+    style = s: pkgs.writeTextFile "caret-root-style" s;
+  in {
+    inherit script style;
+    html = { head, body }: pkgs.writeTextFile "caret-root-doc" ''
+      <!DOCTYPE HTML>
+      <html>
+        <head>
+          ${head}
+          <link rel="stylesheet" href="${url.ofFile style}" />
+          <script type="application/javascript" src="${url.ofFile.script}"></script>
+        </head>
+        <body>
+          ${body}
+        </body>
+      </html>
+    '';
+        
+  };
 } 
 
